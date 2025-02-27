952d1171a2b920e38f61382a07fee358e48a958e
KarasuXYZ <sebastian123456ja@gmail.com>
2025-02-27 04:34:19 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -6,18 +6,21 @@ const form2 = document.getElementById("form2");
 console.log(imie, nazwisko);
 
 form.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); 
     console.log("Imię:", imie.value);
     console.log("Nazwisko:", nazwisko.value);
 });
 
 form2.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); 
     if(imie.value === "Sebastian" && nazwisko.value === "Kramarz") {
-        window.location.href = "Sebastian.html"; // Change to desired URL
+        window.location.href = "Sebastian.html"; 
     }
     else if(imie.value === "Anna" && nazwisko.value === "Kowalska") {
-        window.location.href = "Anna.html"; // Change to desired URL
+        window.location.href = "Anna.html"; 
+    }
+    else if(imie.value === "test" && nazwisko.value === "test") {
+        window.location.href = "test.html"; 
     }
     else {
         alert("Sebuś nie ma dla ciebie wiadomości");
